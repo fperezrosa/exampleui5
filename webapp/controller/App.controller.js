c5f64c8b4f8082b258e633a0a77113a1de1216f1
Fernando Pérez Rosa <fperezrosa@deloitte.es>
2023-07-27 11:28:39 +0000

    cambio dev

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, MessageBox) {
         "use strict";
 
         return Controller.extend("exampleui5.controller.App", {
